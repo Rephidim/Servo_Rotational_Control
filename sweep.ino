e145a7a0d5e91cbee211d7b498421010238d0103
Rephidim <mcpvy@hotmail.com>
2018-04-17 10:25:00 -0600

    Corrected Typos

--- sweep.ino
+++ sweep.ino
@@ -1,58 +1,46 @@
-#include <Servo.h>
+/*
+* Automated Flashpoint Restricted-Angle Oscillator software
+* Marty PoVey
+*/
+#include <VarSpeedServo.h>
 
-Servo servo1;
+VarSpeedServo servo;
+
 
 const int inPin = 2;
-const int servoPin = 9;
+const int angleL = 40;
+const int angleR = 0;
+const int spd = 10;
 
-int state = 1;
+int state = LOW;
 int readState;
-int pos = 0;
-int delayCounter = 0;
-int increment = 1;
-int delayTime = 12000;
+int previousState = LOW;
 
 long time = 0;
-long deBoucne = 500;
+long deBounce = 200;
 
 void setup() {
   pinMode(inPin, INPUT);
-  servo1.attach(servoPin);
-  Serial.begin(9600);
-  servo1.write(0);
-}
-
-int toggleState() {
-  if (state == 1) {
-    state = 0;
-  } else {
-    state = 1;
-  }
-  time = millis();
-}
-
-void runMotor() {
-  if (state == 0) {
-    if (pos >= 40) {
-      increment = -1;
-    } else if (pos <= 0) {
-      increment = 1;
-    }
-    pos += increment;
-    servo.write(pos);
-  }
+  servo.attach(9);
 }
 
 void loop() {
+  
   readState = digitalRead(inPin);
-  delayCounter += 1;
-  
-  if (readState == 0 && millis() - time > deBounce) {
-    toggleState();
+
+  if (readState == HIGH && previousState == LOW && millis() - time > deBounce) {
+    if (state == HIGH)
+      state = LOW;
+    else
+      state = HIGH;
+
+    time = millis();
+  }
+
+  if (state == HIGH) {
+    servo.write(angleL, spd, true);
+    servo.write(angleR,spd,true);
   }
   
-  if (delayCounter >= delayTime) {
-    delayCounter = 0;
-    runMotor();
-  }
+  previousState = readState;
 }
